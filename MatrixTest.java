cfd62427ae43cf147e326e05bbb0ecba3200ceed
jitendra sachdeva <jitendrs@thoughtworks.com>
2016-02-18 18:35:27 +0530

    method for multiply two matrix has been written

--- MatrixTest.java
+++ MatrixTest.java
@@ -1,16 +1,15 @@
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 import matrix.*;
-import java.util.Arrays;
 
 public class MatrixTest {
   @Test
   public void addition_test_for_2_by_2_mtx() {
-  	Matrix metrix = new Matrix(2,2);
-  	metrix.put(10,0,0);
-  	metrix.put(20,0,1);
-  	metrix.put(20,1,0);
-  	metrix.put(10,1,1);
+  	Matrix matrix = new Matrix(2,2);
+  	matrix.put(10,0,0);
+  	matrix.put(20,0,1);
+  	matrix.put(20,1,0);
+  	matrix.put(10,1,1);
 
   	Matrix anotherMatrix = new Matrix(2,2);
   	anotherMatrix.put(1,0,0);
@@ -23,19 +22,19 @@ public class MatrixTest {
   	expectedMtx.put(22,0,1);
   	expectedMtx.put(22,1,0);
   	expectedMtx.put(15,1,1);
-  	Matrix resultMtx = metrix.addWith(anotherMatrix);
+  	Matrix resultMtx = matrix.addWith(anotherMatrix);
   	assertTrue(resultMtx.equals(expectedMtx));
   }
 
   @Test
   public void addition_test_for_2_by_3_mtx() {
-  	Matrix metrix = new Matrix(2,3);
-  	metrix.put(10,0,0);
-  	metrix.put(20,0,1);
-  	metrix.put(5,0,2);
-  	metrix.put(20,1,0);
-  	metrix.put(10,1,1);
-  	metrix.put(6,1,2);
+  	Matrix matrix = new Matrix(2,3);
+  	matrix.put(10,0,0);
+  	matrix.put(20,0,1);
+  	matrix.put(5,0,2);
+  	matrix.put(20,1,0);
+  	matrix.put(10,1,1);
+  	matrix.put(6,1,2);
 
   	Matrix anotherMatrix = new Matrix(2,3);
   	anotherMatrix.put(3,0,0);
@@ -53,18 +52,18 @@ public class MatrixTest {
   	expectedMtx.put(18,1,1);
   	expectedMtx.put(15,1,2);
 
-  	Matrix resultMtx = metrix.addWith(anotherMatrix);
+  	Matrix resultMtx = matrix.addWith(anotherMatrix);
   	assertTrue(resultMtx.equals(expectedMtx));
   }
   @Test
   public void addition_test_for_negative_mtx() {
-  	Matrix metrix = new Matrix(2,3);
-  	metrix.put(-10,0,0);
-  	metrix.put(20,0,1);
-  	metrix.put(-5,0,2);
-  	metrix.put(20,1,0);
-  	metrix.put(10,1,1);
-  	metrix.put(-6,1,2);
+  	Matrix matrix = new Matrix(2,3);
+  	matrix.put(-10,0,0);
+  	matrix.put(20,0,1);
+  	matrix.put(-5,0,2);
+  	matrix.put(20,1,0);
+  	matrix.put(10,1,1);
+  	matrix.put(-6,1,2);
 
   	Matrix anotherMatrix = new Matrix(2,3);
   	anotherMatrix.put(3,0,0);
@@ -82,19 +81,44 @@ public class MatrixTest {
   	expectedMtx.put(18,1,1);
   	expectedMtx.put(3,1,2);
 
-  	Matrix resultMtx = metrix.addWith(anotherMatrix);
+  	Matrix resultMtx = matrix.addWith(anotherMatrix);
   	assertTrue(resultMtx.equals(expectedMtx));
   }
+
+  
+  @Test
+  public void multiplication_test_for_2_by_2_and_2_by_2_matrix() {
+  	Matrix matrix = new Matrix(2,2);
+  	matrix.put(1,0,0);
+  	matrix.put(2,0,1);
+  	matrix.put(2,1,0);
+  	matrix.put(1,1,1);
+
+  	Matrix anotherMatrix = new Matrix(2,2);
+  	anotherMatrix.put(2,0,0);
+  	anotherMatrix.put(1,0,1);
+  	anotherMatrix.put(1,1,0);
+  	anotherMatrix.put(2,1,1);
+
+  	Matrix expectedMtx = new Matrix(2 ,2);
+  	expectedMtx.put(4,0,0);
+  	expectedMtx.put(5,0,1);
+  	expectedMtx.put(5,1,0);
+  	expectedMtx.put(4,1,1);
+
+  	Matrix resultMtx = matrix.multiplyWith(anotherMatrix);
+  	assertTrue(resultMtx.equals(expectedMtx));
+  }  
   
   @Test
   public void multiplication_test_for_2_by_3_and_3_by_2_matrix() {
-  	Matrix metrix = new Matrix(2,3);
-  	metrix.put(1,0,0);
-  	metrix.put(2,0,1);
-  	metrix.put(3,0,2);
-  	metrix.put(4,1,0);
-  	metrix.put(5,1,1);
-  	metrix.put(6,1,2);
+  	Matrix matrix = new Matrix(2,3);
+  	matrix.put(1,0,0);
+  	matrix.put(2,0,1);
+  	matrix.put(3,0,2);
+  	matrix.put(4,1,0);
+  	matrix.put(5,1,1);
+  	matrix.put(6,1,2);
 
   	Matrix anotherMatrix = new Matrix(3,2);
   	anotherMatrix.put(7,0,0);
@@ -110,18 +134,18 @@ public class MatrixTest {
   	expectedMtx.put(139,1,0);
   	expectedMtx.put(154,1,1);
 
-  	Matrix resultMtx = metrix.multiplyWith(anotherMatrix);
+  	Matrix resultMtx = matrix.multiplyWith(anotherMatrix);
   	assertTrue(resultMtx.equals(expectedMtx));
   }  
   @Test
   public void multiplication_test_for_3_by_2_and_2_by_3_matrix() {
-  	Matrix metrix = new Matrix(3,2);
-  	metrix.put(1,0,0);
-  	metrix.put(2,0,1);
-  	metrix.put(3,1,0);
-  	metrix.put(4,1,1);
-  	metrix.put(5,2,0);
-  	metrix.put(6,2,1);
+  	Matrix matrix = new Matrix(3,2);
+  	matrix.put(1,0,0);
+  	matrix.put(2,0,1);
+  	matrix.put(3,1,0);
+  	matrix.put(4,1,1);
+  	matrix.put(5,2,0);
+  	matrix.put(6,2,1);
 
   	Matrix anotherMatrix = new Matrix(2,3);
   	anotherMatrix.put(7,0,0);
@@ -142,7 +166,7 @@ public class MatrixTest {
   	expectedMtx.put(106,2,1);
   	expectedMtx.put(117,2,2);
 
-  	Matrix resultMtx = metrix.multiplyWith(anotherMatrix);
+  	Matrix resultMtx = matrix.multiplyWith(anotherMatrix);
   	assertTrue(resultMtx.equals(expectedMtx));
   }
 }

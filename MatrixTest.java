9725f35fab040e6da7a3efa12706a29aabd8fdfe
jitendra sachdeva <jitendrs@thoughtworks.com>
2016-02-18 12:48:14 +0530

    addition of two metrix function has been written

--- MatrixTest.java
+++ MatrixTest.java
@@ -6,74 +6,95 @@ import java.util.Arrays;
 public class MatrixTest {
   @Test
   public void addition_test_for_2_by_2_mtx() {
-  	Matrix matrix = new Matrix();
-  	int[][] matrix_a = new int[][]{
+  	Matrix a = new Matrix(2,2);
+  	a.populate(10,0,0);
+  	a.populate(20,0,1);
+  	a.populate(20,1,0);
+  	a.populate(10,1,1);
+
+  	int[][] matrix = new int[][]{
   	  { 1, 2},
   	  { 2, 3}
   	};
-  	int[][] matrix_b = new int[][]{
-  	  { 7, 2},
-  	  { 5, 9}
-  	};
   	int[][] expected = new int[][]{
-  	  { 8, 4},
-  	  { 7, 12}
+  	  { 11, 22},
+  	  { 22, 13}
   	};
-  	int[][] result = matrix.addMatrices(matrix_a,matrix_b);
+  	int[][] result = a.addMatrices(matrix);
   	assertTrue(Arrays.deepEquals(result, expected));
   }
 
   @Test
   public void addition_test_for_3_by_3_mtx() {
-  	Matrix matrix = new Matrix();
-  	int[][] matrix_a = new int[3][3];
-  	int[][] matrix_b = new int[3][3];
-  	for (int i = 0;i < matrix_a.length ;i++ ) {
-  		Arrays.fill(matrix_a[i], 7);
-  		Arrays.fill(matrix_b[i], 2);
-  	}
-  	int[][] expected = new int[][]{
-  	  { 9, 9, 9},
-  	  { 9, 9, 9},
-  	  { 9, 9, 9}
+  	Matrix a = new Matrix(3,3);
+  	a.populate(10,0,0);
+  	a.populate(20,0,1);
+  	a.populate(22,0,2);
+
+  	a.populate(2,1,0);
+  	a.populate(6,1,1);
+  	a.populate(2,1,2);
+
+  	a.populate(2,2,0);
+  	a.populate(6,2,1);
+  	a.populate(2,2,2);
+
+  	int[][] matrix = new int[][]{
+  	  { 1, 2, 3},
+  	  { 2, 3, 4},
+  	  { 1, 3, 5}
   	};
-  	int[][] result = matrix.addMatrices(matrix_a,matrix_b);
+  	int[][] expected = new int[][]{
+  	  { 11, 22, 25},
+  	  { 4, 9, 6},
+  	  { 3, 9, 7}
+  	};
+  	int[][] result = a.addMatrices(matrix);
   	assertTrue(Arrays.deepEquals(result, expected));
   }
 
   @Test
-  public void addition_test_for_5_by_5_mtx() {
-  	Matrix matrix = new Matrix();
-  	int[][] matrix_a = new int[5][5];
-  	int[][] matrix_b = new int[5][5];
-  	for (int i = 0;i < matrix_a.length ;i++ ) {
-  		Arrays.fill(matrix_a[i], 1);
-  		Arrays.fill(matrix_b[i], 2);
-  	}
-  	int[][] expected = new int[][]{
-  	  { 3, 3, 3, 3, 3},
-  	  { 3, 3, 3, 3, 3},
-  	  { 3, 3, 3, 3, 3},
-  	  { 3, 3, 3, 3, 3},
-  	  { 3, 3, 3, 3, 3}
+  public void addition_test_for_2_by_3_mtx() {
+  	Matrix a = new Matrix(2,3);
+  	a.populate(10,0,0);
+  	a.populate(20,0,1);
+  	a.populate(22,0,2);
+
+  	a.populate(2,1,0);
+  	a.populate(6,1,1);
+  	a.populate(2,1,2);
+
+  	int[][] matrix = new int[][]{
+  	  { 1, 2, 3},
+  	  { 2, 3, 4}
   	};
-  	int[][] result = matrix.addMatrices(matrix_a,matrix_b);
+  	int[][] expected = new int[][]{
+  	  { 11, 22, 25},
+  	  { 4, 9, 6}
+  	};
+  	int[][] result = a.addMatrices(matrix);
   	assertTrue(Arrays.deepEquals(result, expected));
   }
   @Test
-  public void addition_test_for_2_by_3_mtx() {
-  	Matrix matrix = new Matrix();
-  	int[][] matrix_a = new int[2][3];
-  	int[][] matrix_b = new int[2][3];
-  	for (int i = 0;i < matrix_a.length ;i++ ) {
-  		Arrays.fill(matrix_a[i], 1);
-  		Arrays.fill(matrix_b[i], 2);
-  	}
-  	int[][] expected = new int[][]{
-  	  { 3, 3, 3},
-  	  { 3, 3, 3}
+  public void addition_test_for_negative_mtx() {
+  	Matrix a = new Matrix(2,3);
+  	a.populate(-10,0,0);
+  	a.populate(-20,0,1);
+  	a.populate(22,0,2);
+
+  	a.populate(2,1,0);
+  	a.populate(-6,1,1);
+  	a.populate(2,1,2);
+
+  	int[][] matrix = new int[][]{
+  	  { 1, 2, 3},
+  	  { 2, 3, 4}
   	};
-  	int[][] result = matrix.addMatrices(matrix_a,matrix_b);
+  	int[][] expected = new int[][]{
+  	  { -9, -18, 25},
+  	  { 4, -3, 6}
+  	};
+  	int[][] result = a.addMatrices(matrix);
   	assertTrue(Arrays.deepEquals(result, expected));
   }
 }
--- matrix/Matrix.java
+++ matrix/Matrix.java
@@ -2,22 +2,43 @@ package matrix;
 
 import java.util.Arrays;
 public class Matrix{
-	public int[][] addMatrices(int[][] matrix1, int[][] matrix2){
+	private int[][] matrix;
+	public Matrix(int m, int n){
+		int[][] matrix = new int[m][n];
+		this.matrix = matrix;		
+	};
+
+	public void populate(int number, int m, int n){
+		this.matrix[m][n] = number;
+	};
+
+	public int[][] addMatrices(int[][] matrix1){
 		int[][] result = new int[matrix1.length][matrix1[0].length];
 		for(int i = 0; i < matrix1.length; i++){
 			for(int j = 0; j < matrix1[i].length;j++){
-				result[i][j] = matrix1[i][j] + matrix2[i][j];
+				result[i][j] = matrix1[i][j] + this.matrix[i][j];
 			};
 		};
 		return result;
 	};
-	public int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2){
-		int[][] result = new int[matrix1.length][matrix1[0].length];
-		for(int i = 0; i < matrix1.length; i++){
-			for(int j = 0; j < matrix1[i].length;j++){
-				result[i][j] = matrix1[i][j] + matrix2[i][j];
-			};
-		};
-		return result;
-	};
+	public String string(){
+		String str = "";
+		for (int i = 0;i < this.matrix.length ;i++ ) {
+			str += Arrays.toString(this.matrix[i])+"\n";
+		}
+		return str;
+	}
+	// public int[][] multiplyMatrices(int[][] matrix1, int[][] matrix2){
+	// 	int[][] result = new int[matrix1.length][matrix2[0].length];
+	// 	for (int i = 0;i < matrix1.length ;i++ ) {
+	// 		for(int j = 0; j < matrix1[i].length; j++){
+	// 			for (int k = 0;k < matrix2.length ;k++) {
+	// 				for (int l = 0;l < matrix2[k].length ;l++) {
+	// 					int 
+	// 				}
+	// 			}
+	// 		}
+	// 	}
+	// 	int product = 
+	// };
 }
